ced53ebc2d31e9962091affe926c6e567730a59d
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-04-04 21:30:09 +0200

    baseline project login

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
